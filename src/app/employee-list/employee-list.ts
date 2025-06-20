6d5da6390388149e26d9546f147bcb83c84522e2
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-20 15:55:04 +0530

    designed list emp

--- src/app/employee-list/employee-list.ts
+++ src/app/employee-list/employee-list.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css'
 })
 export class EmployeeList {
+
+  employees!: Employee[];
+
+  ngOnInit(): void{
+    this.employees = [{
+      "empUserId": 1,
+      "empFirstName": "john",
+      "empLastName": "will",
+      "empEmailId": "jwill@co.in"
+    },{
+           "empUserId": 2,
+      "empFirstName": "peter",
+      "empLastName": "davidson",
+      "empEmailId": "pdavidson@co.in"
+    }];
+  }
 
 }
