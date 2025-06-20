e9e03f93b932219dce93926da1ed6fca301802c1
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-20 15:17:35 +0530

    fixed routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeList } from "./employee-list/employee-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'angular-frontend';
 }
--- src/app/employee-list/employee-list.ts
+++ src/app/employee-list/employee-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-employee-list',
+  standalone: true,
   imports: [],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css'
