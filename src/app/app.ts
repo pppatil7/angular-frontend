7d197c42234bf4ba318cec7b5569aa15c67e2a28
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-23 14:13:46 +0530

    updated app ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeList } from "./employee-list/employee-list";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeList],
+  standalone: true,
+  imports: [RouterOutlet, EmployeeList,HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
